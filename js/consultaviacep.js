9344e800160fdcb300f12d68d86784ce79400616
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-08-03 11:15:17 -0300

    Aula 03/08 HTML/JS

--- js/consultaviacep.js
+++ js/consultaviacep.js
@@ -1,13 +1,62 @@
-let prompt = require('prompt-sync')();
+// let prompt = require('prompt-sync')();
 
-let cep = prompt('Informe o CEP: ');
+// let cep = prompt('Informe o CEP: ');
 
-buscarCep(cep);
+// buscarCep(cep);
 
-async function buscarCep(cep){
-    const promiseConsultaCep = await fetch('https://viacep.com.br/ws/' + cep + '/json');
+async function buscarCep(){
+    limpar();
+    //Parametros opcionais para a requisição
+    let options = {
+        method: "GET",
+        headers: {"Content-type": "application/json"}
+    };
 
-    const json = await promiseConsultaCep.json();
+    var txtCep = document.getElementById('txtCep');
+    var cepInformado = txtCep.value;
     
-    console.log(json);
+    //Esta variável é uma Promise(um objeto que será preenchido quando a requisição HTTP retornar)
+    fetch('https://viacep.com.br/ws/' + cepInformado + '/json')
+    .then(resultado => resultado.json())
+    .then(json => {
+        if(json.erro){
+            limpar();
+            mostrarTelDeErro();
+        } else {
+            preencherCamposComJson(json);
+        }
+    })
+    .catch(erro => {
+        mostrarTelDeErro();
+    })
+}
+
+function preencherCamposComJson(json){
+    if(json.bairro){
+        txtBairro.value = json.bairro;
+    }else{
+        txtBairro.disabled = false;
+    }
+    txtLocalidade.value = json.localidade;
+    txtLogradouro.value = json.logradouro;
+    txtUf.value = json.uf;
+}
+
+function mostrarTelDeErro(){
+    limpar();
+    document.getElementById('divDeTudo').style.backgroundColor = "red";
+    alert('CEP informado não existe')
+}
+
+function limpar(){
+    document.getElementById('divDeTudo').style.backgroundColor = "white";
+    txtBairro.value = '';
+    txtLocalidade.value = '';
+    txtLogradouro.value = '';
+    txtUf.value = '';
+
+    txtBairro.disabled = true;
+    txtLocalidade.disabled = true;
+    txtLogradouro.disabled = true;
+    txtUf.disabled = true;
 }
