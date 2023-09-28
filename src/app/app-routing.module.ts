c6ff105a865ca7a1e64eda86360bf83ece0c24b4
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-09-28 11:07:10 -0300

    Commit aula 28/09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutosModule } from './produtos/produtos.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo:'produtos/lista', pathMatch:'full'},
+  {
+    path: 'produtos',
+    loadChildren:() => import('./produtos/produtos.module').then(m=>m.ProdutosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/produtos/produto-listagem/produto-listagem.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-listagem',
+  templateUrl: './produto-listagem.component.html',
+  styleUrls: ['./produto-listagem.component.scss']
+})
+
+
+
+export class ProdutoListagemComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
