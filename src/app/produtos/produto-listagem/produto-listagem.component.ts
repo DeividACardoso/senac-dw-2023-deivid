e5d249a8b4d85907ab1ebeec0804bd46f1faecf1
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-10-03 11:06:13 -0300

    Aula 03/10 Conversando com o Backend

--- src/app/produtos/produto-listagem/produto-listagem.component.ts
+++ src/app/produtos/produto-listagem/produto-listagem.component.ts
@@ -1,4 +1,6 @@
+import { ProdutoService } from './../../shared/service/produto.service';
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/shared/model/seletor/produto';
 
 @Component({
   selector: 'app-produto-listagem',
@@ -9,8 +11,29 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class ProdutoListagemComponent implements OnInit{
+
+  public produtos: Array<Produto> = new Array();
+
+  constructor(private produtoService: ProdutoService){
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.buscarProdutos();
+  }
+
+  editar(id: number){
+// TODO Implementar
+  }
+  buscarProdutos(){
+    this.produtoService.listarTodos().subscribe(
+      resultado => {
+        this.produtos = resultado;
+      },
+      erro => {
+        console.log('Erro ao buscar Produtos', erro);
+      }
+    )
+
   }
 
 }
